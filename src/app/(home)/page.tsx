9f1510cace9c29218a715100b80229e84c92ee20
PatrickEN-dev <patrickandreia2505@gmail.com>
2023-10-20 21:10:26 -0300

    feat: add first banner to home-page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-5">
+      <Image
+        className="h-auto w-full"
+        sizes="100vw"
+        src="/banner.svg"
+        alt="Até 55% de desconto esse mês"
+        width={0}
+        height={0}
+      />
+    </main>
+  );
+}
