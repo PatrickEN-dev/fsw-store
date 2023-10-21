c26428b3f4131e4784e7d572c989fdb49fb211e6
PatrickEN-dev <patrickandreia2505@gmail.com>
2023-10-21 14:15:34 -0300

    feat: create ProductImages component

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,11 +1,19 @@
 import { IslugParams } from "@/@types/globals";
 import { getProductBySlug } from "@/helpers/getProductPrisma";
+import ProductImages from "./components/ProductImage";
 
 const ProductDetailPage = async ({ params: { slug } }: IslugParams) => {
   const productBySlug = await getProductBySlug(slug);
 
   if (!productBySlug) return null;
-  return <div>{productBySlug.name}</div>;
+  return (
+    <div>
+      <ProductImages
+        name={productBySlug.name}
+        imageUrls={productBySlug.imageUrls}
+      />
+    </div>
+  );
 };
 
 export default ProductDetailPage;
