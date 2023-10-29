18e2fbd6c852d5e87e34f90436f8980329497080
PatrickEN-dev <patrickandreia2505@gmail.com>
2023-10-29 01:15:38 -0300

    fix: dynamic ordersPage

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -6,6 +6,8 @@ import { getServerSession } from "next-auth";
 import OrderList from "./components/lists/OrderList";
 import UserNotFound from "@/components/UserNotFound";
 
+export const dynamic = "force-dynamic";
+
 const OrderPage = async () => {
   const session = await getServerSession(authOptions);
 
