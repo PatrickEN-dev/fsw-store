4e1c59720d6ff0345d9459b72437351c4fb92a86
PatrickEN-dev <patrickandreia2505@gmail.com>
2023-10-31 20:28:00 -0300

    feat: add heading variant to badge shadcn component

--- src/app/Catalog/page.tsx
+++ src/app/Catalog/page.tsx
@@ -1,16 +1,13 @@
-import { Badge } from "@/components/ui/badge";
-import { ShapesIcon } from "lucide-react";
 import { getCategories } from "@/helpers/getCategoryPrisma";
 import CategoryItem from "./components/CategoryItem";
+import { Badge } from "@/components/ui/badge";
+import { ShapesIcon } from "lucide-react";
 
 const CatalogPage = async () => {
   const categories = await getCategories();
   return (
     <main className="flex flex-col gap-8 p-5">
-      <Badge
-        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
-        variant={"outline"}
-      >
+      <Badge variant="heading">
         <ShapesIcon size={16} />
         Catálogo
       </Badge>
